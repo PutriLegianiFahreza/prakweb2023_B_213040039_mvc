1c43662f33dcb672101f4839386c5bbb2fad0878
PutriLegianiFahreza <putrilegiani234@gmail.com>
2023-10-22 15:49:19 +0700

    #6 Assets

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -2,6 +2,6 @@
 
     <h1 class="mt-4">About Me</h1>
     <img src="<?= BASEURL; ?>/img/profile.jpeg" alt="Putri Legiani" width="200" class="rounde-circle shadow">
-    <p>Haloo, nama saya <?= $data['nama'];?>, umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?=$data['pekerjaan']; ?> </p>
+    <p>Halo, nama saya <?= $data['nama'];?>, umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?=$data['pekerjaan']; ?> </p>
 
     </div>
