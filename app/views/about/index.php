354433fc8e9ba223bc8c28580661d261bfd277e1
PutriLegianiFahreza <putrilegiani234@gmail.com>
2023-10-22 15:40:56 +0700

    #5 views

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,7 @@
+<div class="container">
 
-    <h1>About Me</h1>
+    <h1 class="mt-4">About Me</h1>
+    <img src="<?= BASEURL; ?>/img/profile.jpeg" alt="Putri Legiani" width="200" class="rounde-circle shadow">
     <p>Haloo, nama saya <?= $data['nama'];?>, umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?=$data['pekerjaan']; ?> </p>
+
+    </div>
--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -3,5 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <title>Halaman <?= $data['judul']; ?></title>
+    <link rel="stylesheet" href="<?= BASEURL; ?>/css/bootsrap.css">
 </head>
 <body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container">
+  <a class="navbar-brand" href="<?= BASEURL; ?>">PHP MVC</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <div class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="<?= BASEURL; ?>">Home <span class="sr-only">(current)</span></a>
+      </li>
+        <a class="nav-item nav-link" href="<?= BASEURL; ?>/about">About</a>
+</div>
+  </div>
+  </div>
+</nav>
